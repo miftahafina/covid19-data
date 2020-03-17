12abbefb6b82f034fb982ce74db068a2098cde45
Miftah Afina <miftah.afina@gmail.com>
2020-03-17 14:21:33 +0700

    Add basic routing

--- src/Containers/App/App.jsx
+++ src/Containers/App/App.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import 'normalize.css';
 import './App.css';
 
 import Header from '../../Components/Header/Header';
-// import CounterPage from '../CounterPage/CounterPage';
+import CounterPage from '../CounterPage/CounterPage';
 import ChartPage from '../ChartPage/ChartPage';
 
 const App = () => {
   return (
-    <div className="container">
-      <Header />
-      {/* <CounterPage /> */}
-      <ChartPage />
-    </div>
+    <Router>
+      <div className="container">
+        <Header />
+
+        <Switch>
+          <Route path="/chart">
+            <ChartPage />
+          </Route>
+          <Route exact path="/">
+            <CounterPage />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
