a1e4b7a2d6ed0b9dbb346064e5ee2de8d3991d19
Miftah Afina <miftah.afina@gmail.com>
2020-03-17 10:55:09 +0700

    Change index.css to App.css

--- src/Containers/App/App.jsx
+++ src/Containers/App/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import 'normalize.css';
+import './App.css';
 
 import Header from '../../Components/Header/Header';
 import CounterPage from '../CounterPage/CounterPage';
